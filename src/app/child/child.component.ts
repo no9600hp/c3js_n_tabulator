0a79849910ad531c90936db61d325aaf0cf64cbd
Minhaz Uddin <minhaz@httechus.com>
2019-05-14 11:39:18 -0500

    c3 chart works

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -8,30 +8,13 @@ import * as c3 from 'c3';
 export class ChildComponent implements OnInit {
   data1= ['data1', 30, 200, 100, 400, 150, 250, 30]
   data2 = ['data2', 130, 100, 140, 200, 150, 50, 400]
+  data3 = ['data3', 180, 10, 100, 100, 120, 30, 300]
   constructor() { }
 
   ngOnInit() {
-    this.createChart();
+    
   }
 
 
-  createChart(){
-    var chart = c3.generate({
-      bindto: '#barChart',
-      data: {
-          columns: [
-              this.data1,
-              this.data2
-          ],
-          type: 'bar'
-      },
-      bar: {
-          width: {
-              ratio: 0.5 // this makes bar width 50% of length between ticks
-          }
-          // or
-          //width: 100 // this makes bar width 100px
-      }
-  });
-  }
+
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import * as c3 from 'c3';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss']
 })
 export class ParentComponent implements OnInit {
+  @Input() data1
+  @Input() data2
+  @Input() data3
+  
 
   constructor() { }
 
   ngOnInit() {
+    this.createChart();
+  }
+
+    createChart(){
+    var chart = c3.generate({
+      bindto: '#barChart',
+      data: {
+          columns: [
+              this.data1,
+              this.data2,
+              this.data3
+          ],
+          type: 'bar'
+      },
+      bar: {
+          width: {
+              ratio: 0.5 // this makes bar width 50% of length between ticks
+          }
+          // or
+          //width: 100 // this makes bar width 100px
+      }
+  });
   }
 
 }
